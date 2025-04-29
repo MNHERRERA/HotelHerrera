a728b1483272aefb3b0356afc733d6e7176c0a86
MateoHerrera <mateo.herrera@udla.edu.ec>
2025-04-29 07:46:53 -0500

    Modelo clientes

---
+++ HotelHerrera/Models/Clientes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HotelHerrera.Models
+{
+    public class Clientes
+    {
+        [Key]
+        [MaxLength(15)]
+        public string Nombre { set; get; }
+        [MaxLength(15)]
+        public string Telefono { set; get; }
+        public int Id { set; get; }
+        public DateTime Cumpleaños {set;get;}
+        
+        public bool Acompanantes { set; get; } 
+       
+
+        
+
+    }
+}
